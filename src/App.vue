f84c070099f22043781f0c0d48642fd06825d51b
Jonas Peterburs <j.peterburs@protonmail.com>
2025-03-18 19:52:08 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1 v-if="remainingHours <= 24">The Final Day</h1>
+  <h2>- {{ remainingHours }} Hour{{ remainingHours == 1 ? '' : 's' }} Remain -</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      remainingHours: this.calculateRemainingHours(),
+    }
+  },
+  methods: {
+    calculateRemainingHours() {
+      const now = new Date()
+      const theDay = new Date('2025-04-01')
+
+      return Math.floor((theDay - now) / 1000 / 60 / 60)
+    },
+    updateCountdown() {
+      this.remainingHours = this.calculateRemainingHours()
+    },
+  },
+  mounted() {
+    this.updateCountdown()
+    this.timer = setInterval(this.updateCountdown, 60 * 1000)
+  },
+  beforeUnmount() {
+    clearInterval(this.timer)
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 7rem;
+  margin: 0;
+}
+
+h2 {
+  font-size: 3rem;
+  font-weight: normal;
+}
+</style>
